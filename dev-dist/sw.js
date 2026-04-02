7bde991997cff63515f659e21dae374bae12428c
Jafet-Gamboa <0322103712@ut-tijuana.edu.mx>
2026-04-01 19:39:15 -0700

    Union

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.080m7ac5pm8"
+    "revision": "0.oltm35h54qo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
