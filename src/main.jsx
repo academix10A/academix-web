7d6fc34730778d650a1bd0c3db3d93fcb614f73e
Jafet-Gamboa <0322103712@ut-tijuana.edu.mx>
2026-04-01 19:13:27 -0700

    Avance

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <PayPalScriptProvider options={{ clientId: "AYFuaQtsQeteSBIQAMrb_8z-MUP1zft_gt5m6igebK8fuXhLunfdF106KKUq-jvIA9V9t4HeRIaXqHJs", currency: "MXN" }}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </PayPalScriptProvider>,
 )
